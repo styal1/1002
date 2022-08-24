f6137bd886baf710cf20548b5e1dd7456963ba1a
styal1 <111949261+styal1@users.noreply.github.com>
2022-08-24 17:21:07 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
+today = datetime.now()
+start_date ='2022-02-22'
+city ='抚顺'
+birthday ='02-05'
+
+app_id ='wx13a97a0b9efb18f7'
+app_secret ='46a1f433efcfda0f53a8d1bab134248a'
+
+user_id ='o0QhR6fICM-2gpyRrR61j1oeDtbQ'
+template_id ='q9Gd3PZ4SqqP7njxnQ3nDYipR8TM9-jCstrLZDzw28s'
+
+
+def get_weather():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  return weather['weather'], math.floor(weather['temp'])
+
+def get_count():
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
+
+def get_birthday():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_words():
+  words = requests.get("https://api.shadiao.pro/chp")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+wea, temperature = get_weather()
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+print(res)
